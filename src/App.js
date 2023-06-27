ef327f55b1ebbfe7f9e7089a5f75db392763cfe0
Mateen <zahidx297@yahoo.com>
2023-06-27 19:56:43 -0400

    add template for homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import Homepage from './Homepage.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Homepage />
       </header>
     </div>
   );
