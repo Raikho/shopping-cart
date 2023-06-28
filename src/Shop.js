c69b55a40f47b4f1540c59c6bb95664b2c10fc51
Mateen <zahidx297@yahoo.com>
2023-06-27 23:30:05 -0400

    add basic items to shop to template

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,31 @@
+
+import uniqid from 'uniqid';
+import { useState } from 'react';
+
 const Shop = () => {
+    const [items, setItems] = useState([
+        {name: 'shirt', id: uniqid()},
+        {name: 'pants', id: uniqid()},
+        {name: 'jacket', id: uniqid()},
+        {name: 'sweater', id: uniqid()},
+        {name: 'socks', id: uniqid()},
+        {name: 'belt', id: uniqid()},
+    ]);
+
     return (
-        <div className='shop'>Shop</div>
+        <div className='shop'>
+            <div className='title'>Shop</div>
+            <div className="item-container">
+                {items.map(item => 
+                    <div 
+                        className='item'
+                        key={item.id}
+                    >
+                        {item.name}
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
 
