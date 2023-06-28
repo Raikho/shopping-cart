10777e9d5255af673722e804e4462a6b406b3351
Mateen <zahidx297@yahoo.com>
2023-06-27 23:45:29 -0400

    add contact to nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Homepage from './Homepage.js';
 import Shop from './Shop.js';
+import Contact from './Contact.js';
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
             <div className="nav-container">
               <NavLink to="/">Home</NavLink>
               <NavLink to="shop">Shop</NavLink>
+              <NavLink to="contact">Contact</NavLink>
             </div>
             <div className="cart">Cart</div>
           </div>
           <Routes>
             <Route path='/' element={<Homepage />} />
             <Route path='shop' element={<Shop />} />
+            <Route path='contact' element={<Contact />} />
           </Routes>
         </BrowserRouter>
       </header>
