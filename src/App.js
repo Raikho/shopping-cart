d9b6f7690f063ad89c9d3ddc4e0b85700dd8318b
Mateen <zahidx297@yahoo.com>
2023-06-30 19:02:43 -0400

    add quantity to items array, update addItem/setTotal

--- src/App.js
+++ src/App.js
@@ -10,25 +10,35 @@ import MainLayout from './MainLayout.js';
 import ShopLayout from './ShopLayout.js';
 import ItemList from './ItemList.js';
 
+const baseItems = [
+  {name: 'Betta', cat: 'fresh', price: 9, id: uniqid(), img: 'betta', quantity: 0},
+  {name: 'Oscar', cat: 'fresh', price: 3, id: uniqid(), img: 'oscar', quantity: 0},
+  {name: 'Discus', cat: 'fresh', price: 5, id: uniqid(), img: 'discus', quantity: 0},
+  {name: 'Clownfish', cat: 'salt', price: 3, id: uniqid(), img: 'clown-fish', quantity: 0},
+  {name: 'Blue Tang', cat: 'salt', price: 4, id: uniqid(), img: 'blue-tang', quantity: 0},
+  {name: 'Angel Fish', cat: 'salt', price: 6, id: uniqid(), img: 'angel-fish', quantity: 0},
+  {name: 'Fish Bowl', cat: 'supplies', price: 18, id: uniqid(), img: 'fish-bowl', quantity: 0},
+  {name: 'Decoration', cat: 'supplies', price: 10, id: uniqid(), img: 'decoration', quantity: 0},
+];
+
 function App() {
-  const [items, setItems] = useState([
-    {name: 'Betta', cat: 'fresh', price: 9, id: uniqid(), img: 'betta'},
-    {name: 'Oscar', cat: 'fresh', price: 3, id: uniqid(), img: 'oscar'},
-    {name: 'Discus', cat: 'fresh', price: 5, id: uniqid(), img: 'discus'},
-    {name: 'Clownfish', cat: 'salt', price: 3, id: uniqid(), img: 'clown-fish'},
-    {name: 'Blue Tang', cat: 'salt', price: 4, id: uniqid(), img: 'blue-tang'},
-    {name: 'Angel Fish', cat: 'salt', price: 6, id: uniqid(), img: 'angel-fish'},
-    {name: 'Fish Bowl', cat: 'supplies', price: 18, id: uniqid(), img: 'fish-bowl'},
-    {name: 'Decoration', cat: 'supplies', price: 10, id: uniqid(), img: 'decoration'},
-  ]);
+  const [items, setItems] = useState(baseItems);
 
   const [total, setTotal] = useState(0);
 
   const handleAddItem = id => {
-    let item = items.filter(item => item.id === id)[0];
-    console.log('trying to add item:', item);
-    setTotal(total + item.price);
+    setItems(prevItems => {
+      let newItems = JSON.parse(JSON.stringify(prevItems));
+      let item = newItems.filter(item => item.id === id)[0];
+      item.quantity++;
+      return newItems;
+    });
   }
+
+  useEffect(() => {
+    let newTotal = items.reduce((prev, cur) => prev + (cur.price * cur.quantity), 0);
+    setTotal(newTotal);
+  }, [items]);
 
   const getItems = cat => {
     if (cat === 'all') return items;
