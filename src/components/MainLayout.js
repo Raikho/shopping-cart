d2158d34906f629310a5f411a1b409b992a60990
Mateen <zahidx297@yahoo.com>
2023-07-14 17:15:27 -0400

    add github link to footer

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, Link } from 'react-router-dom';
 import Header from './Header.js';
 
 const MainLayout = props => {
@@ -7,11 +7,15 @@ const MainLayout = props => {
     
     if (path === '/') pathClass = ' home';
     else if (path.startsWith('/shop')) pathClass = ' shop'
+
     return (
         <div className={'main-layout' + pathClass}>
             <Header total={props.total}/>
             <Outlet />
-            <div className="footer">Footer</div>
+            <div className="footer">
+                <a href='https://github.com/Raikho/shopping-cart' className="icon git">
+                </a>
+            </div>
         </div>
     )
 }
