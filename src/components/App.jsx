6868ff711ab16396489ed41272fb3395b7459fc5
Mateen <zahidx297@yahoo.com>
2023-07-21 08:23:13 -0400

    add more advanced testing options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,25 @@ import './App.css'
 function App() {
 
   const arr = [];
-  for (let i = 0; i < 14; i++) {
+  for (let i = 0; i < 14; i++)
     arr.push(i);
-  }
+
+  let isTesting = true;
+  const [testHeading, setTestHeading] = useState('Test Heading');
+  const clickHandler = () => {setTestHeading('Radical Rhinos');};
 
   return (
     <div className="App">
-      <div className="header">Header</div>
+      
+      {(isTesting) ? 
+        <div className="testing">
+          <h1>{testHeading}</h1>
+          <button type="button" onClick={clickHandler}>Click Me</button>
+        </div> :
+        null
+      }
+
+      <div className="header"></div>
       <div className="content">
         {arr.map(item => (
           <div 
--- src/components/tests/App.test.jsx
+++ src/components/tests/App.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('something truthy and falsy', () => {
@@ -8,9 +9,23 @@ describe('something truthy and falsy', () => {
     });
 });
 
-describe('App', () => {
-    it('renders headline', () => {
-        render(<App title='React' />);
-        screen.debug();
+describe('testing App component', () => {
+    it('renders', () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot();
+    });
+    it('renders correct heading', () => {
+        render(<App />);
+        expect(screen.getByRole("heading").textContent)
+            .toMatch(/test heading/i);
+    });
+    it('changes heading upon click', async() => {
+        const user = userEvent.setup();
+
+        render(<App />);
+        const button = screen.getByRole("button", { name: "Click Me" });
+        await user.click(button);
+        expect(screen.getByRole("heading").textContent)
+            .toMatch(/radical rhinos/i);
     });
 });
