19fc92bc1d4a482bb1e0e487ba4220025451d4b7
Mateen <zahidx297@yahoo.com>
2023-07-23 22:50:03 -0400

    replace uniqid with uuid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,22 @@
 import { useState, useEffect } from 'react';
-// import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 
-import uniqid from 'uniqid';
 import './App.css';
-// import Header from './Header' // TODO: remove
-import ItemList from './ItemList' // TODO: remove
+import ItemList from './ItemList'
 import MainLayout from './MainLayout';
 import Homepage from './Homepage';
 import ShopLayout from  './ShopLayout';
 
 const startingItems = [
-  {name: 'Betta', cat: 'fresh', price: 9, id: uniqid(), img: 'betta', quantity: 0},
-  {name: 'Oscar', cat: 'fresh', price: 3, id: uniqid(), img: 'oscar', quantity: 0},
-  {name: 'Discus', cat: 'fresh', price: 5, id: uniqid(), img: 'discus', quantity: 0},
-  {name: 'Clownfish', cat: 'salt', price: 3, id: uniqid(), img: 'clown-fish', quantity: 0},
-  {name: 'Blue Tang', cat: 'salt', price: 4, id: uniqid(), img: 'blue-tang', quantity: 0},
-  {name: 'Angel Fish', cat: 'salt', price: 6, id: uniqid(), img: 'angel-fish', quantity: 0},
-  {name: 'Fish Bowl', cat: 'supplies', price: 18, id: uniqid(), img: 'fish-bowl', quantity: 0},
-  {name: 'Decoration', cat: 'supplies', price: 10, id: uniqid(), img: 'decoration', quantity: 0},
+  {name: 'Betta', cat: 'fresh', price: 9, id: uuid(), img: 'betta', quantity: 0},
+  {name: 'Oscar', cat: 'fresh', price: 3, id: uuid(), img: 'oscar', quantity: 0},
+  {name: 'Discus', cat: 'fresh', price: 5, id: uuid(), img: 'discus', quantity: 0},
+  {name: 'Clownfish', cat: 'salt', price: 3, id: uuid(), img: 'clown-fish', quantity: 0},
+  {name: 'Blue Tang', cat: 'salt', price: 4, id: uuid(), img: 'blue-tang', quantity: 0},
+  {name: 'Angel Fish', cat: 'salt', price: 6, id: uuid(), img: 'angel-fish', quantity: 0},
+  {name: 'Fish Bowl', cat: 'supplies', price: 18, id: uuid(), img: 'fish-bowl', quantity: 0},
+  {name: 'Decoration', cat: 'supplies', price: 10, id: uuid(), img: 'decoration', quantity: 0},
 ];
 
 export default function App({ baseItems = startingItems }) {
